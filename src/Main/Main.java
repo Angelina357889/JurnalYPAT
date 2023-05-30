9d86bd6060a1a86d101c9e444a05817f22e3b8a8
Pudkova130 <angel.pudkova@yandex.ru>
2023-05-30 11:40:29 +0300

    Устанавила драйвер подключения к БД видио: https://www.youtube.com/watch?v=BJ2SCZEkKoQ 14 минута

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,14 @@
 package Main;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.SQLException;
 
+import static Main.ConfigDataBase.DateBaseConnection.getDbConnect;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, SQLException {
+        System.out.println(getDbConnect());
     }
+
+
+
 }
